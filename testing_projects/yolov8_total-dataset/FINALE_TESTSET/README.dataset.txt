20b9011d184d168e164e35dc7a25665336bcb10d
Thijs Alens <thijs.alens@telenet.be>
2026-05-05 09:43:34 +0200

    Testing done on new and final test-set

---
+++ testing_projects/yolov8_total-dataset/FINALE_TESTSET/README.dataset.txt
@@ -0,0 +1,6 @@
+# Trash Detection > 2026-04-27 6:29pm
+https://universe.roboflow.com/zakdetectieobb/trash-detection-zhriv
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- testing_projects/yolov8_total-dataset/config.py
+++ testing_projects/yolov8_total-dataset/config.py
@@ -1,3 +1,3 @@
-PATH_TO_BEST_MODEL = '../../runs/detect/train3/weights/best.pt'
+PATH_TO_BEST_MODEL = '../../testing_projects/Yolo-taco_test_v1/runs/train/yolo11s_garbage_5c-2/weights/best.pt'
 
-TEST_DIR_PATH = '../../testing_projects/yolov8_total-dataset/own_testset_v2/train/images'
+TEST_DIR_PATH = '../../testing_projects/yolov8_total-dataset/FINALE_TESTSET/train/images'
